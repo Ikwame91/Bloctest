82f6a2a74130b7aa229d7c5eb023ef09c86868a7
Kwame Agyapong <Kwameagyapong91@gmail.com>
2023-10-22 00:11:14 +0000

    making the countercubit accessible globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/logic/cubit/counter_cubit_cubit.dart';
 import 'package:flutter_application_1/presentation/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   final AppRouter _appRouter = AppRouter();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      onGenerateRoute: _appRouter.onGenerateRoute,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        onGenerateRoute: _appRouter.onGenerateRoute,
+      ),
     );
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/logic/cubit/counter_cubit_cubit.dart';
 import 'package:flutter_application_1/presentation/screens/home_screen.dart';
 import 'package:flutter_application_1/presentation/screens/second_screen.dart';
 import 'package:flutter_application_1/presentation/screens/third_screen.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
   MaterialPageRoute? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child:
-                const HomeScreen(title: 'HomeScreen', color: Colors.blueAccent),
-          ),
+          builder: (_) =>
+              const HomeScreen(title: 'HomeScreen', color: Colors.blueAccent),
         );
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-              value: _counterCubit,
-              child: const SecondScreen(
-                  title: 'SecondScreen', color: Colors.redAccent)),
+          builder: (_) => const SecondScreen(
+              title: 'SecondScreen', color: Colors.redAccent),
         );
       case '/third':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-              value: _counterCubit,
-              child: const ThirdScreen(
-                  title: 'ThirdScreen', color: Colors.greenAccent)),
+          builder: (_) => const ThirdScreen(
+              title: 'ThirdScreen', color: Colors.greenAccent),
         );
       default:
         return null;
